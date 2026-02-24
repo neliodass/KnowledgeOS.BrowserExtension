4d47c18a3c67b9cc24ec44a713a95aba93cee1d1
Bartek Pieróg <pierogbartoszzstti@gmail.com>
2026-02-24 15:39:17 +0100

    add Tailwind CSS and PostCSS configuration with initial styles

--- popup.tsx
+++ popup.tsx
@@ -1,22 +1,25 @@
+import "~/style.css"
+
 import { useState } from "react"
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
+    <div className="p-4">
+      <h2 className="text-lg font-semibold mb-2">
         Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
+        <a href="https://www.plasmo.com" target="_blank" className="text-blue-600 underline">
           Plasmo
         </a>{" "}
         Extension!
       </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
+      <input
+        className="border border-gray-300 rounded px-2 py-1 mb-2 w-full"
+        onChange={(e) => setData(e.target.value)}
+        value={data}
+      />
+      <a href="https://docs.plasmo.com" target="_blank" className="text-blue-600 underline">
         View Docs
       </a>
     </div>
